31ac98772139343023d9a03b3a3f736af442d06d
Dzukito <30675879+Dzukito@users.noreply.github.com>
2022-05-17 13:35:12 -0300

    AutoPublic

--- AutoPublic_FullAuto.py
+++ AutoPublic_FullAuto.py
@@ -1,3 +1,5 @@
+
+   
 from urllib import request
 import pandas as pd #pip install pandas
 pd.options.mode.chained_assignment = None  #Sirve para quitar un warning molesto
@@ -34,33 +36,37 @@ def feodo():
   #Abro el archivo de Feodo como dataframe
   data=pd.read_csv(local_file,skiprows=8)[:-1] #skiprows para eliminar los detalles de feodo y [:-1] para eliminar el #END 
 
-  #lo filtro por fecha
- 
-  dataHoy=data[(data.first_seen_utc.str[:4]=="2022") & (data.first_seen_utc.str[5:7]==mes) & (data.first_seen_utc.str[8:10]==dia)]
+  #los IOC de hoy
+  dataHoy=data[(data.first_seen_utc.str[:4]==anio) & (data.first_seen_utc.str[5:7]==mes) & (data.first_seen_utc.str[8:10]==dia)]
   
+  #si es lunes, traeme los IOCs de el sabado y el domingo
   if(datetime.datetime.today().weekday()==0):
-    dataDomingo=data[(data.first_seen_utc.str[:4]=="2022") & (data.first_seen_utc.str[5:7]==mes)  & (data.first_seen_utc.str[8:10]==str(int(dia)-1))]
-    dataSabado=data[(data.first_seen_utc.str[:4]=="2022") & (data.first_seen_utc.str[5:7]==mes)  & (data.first_seen_utc.str[8:10]==str(int(dia)-2))]
+    dataDomingo=data[(data.first_seen_utc.str[:4]==anio) & (data.first_seen_utc.str[5:7]==mes)  & (data.first_seen_utc.str[8:10]==str(int(dia)-1))]
+    dataSabado=data[(data.first_seen_utc.str[:4]==anio) & (data.first_seen_utc.str[5:7]==mes)  & (data.first_seen_utc.str[8:10]==str(int(dia)-2))]
     dataAyer=pd.concat([dataDomingo,dataSabado])
-  else:
-    dataAyer=data[(data.first_seen_utc.str[:4]=="2022") & (data.first_seen_utc.str[5:7]==mes)  & (data.first_seen_utc.str[8:10]==str(int(dia)-1)) & (data.first_seen_utc.str[10:13].astype(int)>int(hora))]
 
+  #si es dia de semana, traeme los de ayer, ya sea: si lo ejecute a las 10:20, los que se subieron de 10:20 a 11:00 y de 11:00 a el fin del dia.
+  else:
+    dataAyerOtrasHoras=data[(data.first_seen_utc.str[:4]==anio) & (data.first_seen_utc.str[5:7]==mes)  & (data.first_seen_utc.str[8:10]==str(int(dia)-1)) & (data.first_seen_utc.str[10:13].astype(int)>int(hora))]
+    dataAyerOtrosMinutos=data[(data.first_seen_utc.str[:4]==anio) & (data.first_seen_utc.str[5:7]==mes)  & (data.first_seen_utc.str[8:10]==str(int(dia)-1)) & (data.first_seen_utc.str[10:13].astype(int)==int(hora)) & (data.first_seen_utc.str[14:16].astype(int)>int(minutos))]
+    dataAyer=pd.concat([dataAyerOtrasHoras,dataAyerOtrosMinutos])
+    
   dataRequerida=pd.concat([dataHoy,dataAyer])
 
-
+  #si no obtuve ningun IOC
   if(dataRequerida.empty):
     print("<!>: No se obtuvieron IOCs en Feodo. Checkear que se haya usado formato mm o dd para el input o revisar manualmente el CSV para verificar que no hay resultados.")
     return pd.DataFrame(columns=['IOC', 'Type', 'Family', 'Source'])
 
   else:  
-    #Tomo las columnas de IP y Family que son las que me importan
+    #Tomo las columnas de IP y Family del CSV que son las que me importan
     ipsLimpias=dataRequerida[["dst_ip","malware"]]
 
     #Le agrego una columna source y otra IP
     ipsLimpias["Type"]="IP"
     ipsLimpias["Source"]="https://feodotracker.abuse.ch/blocklist/#iocs"
 
-    #Las ordeno para que quede bonito a como quiero
+    #Las ordeno para que quede bonito el orden a como quiero
     ipsLimpias.rename(columns = {'dst_ip':'IOC', 'malware':'Family'}, inplace = True)
     ipsLimpias = ipsLimpias[['IOC', 'Type', 'Family', 'Source']]
     
@@ -93,7 +99,7 @@ def scrapearTabla(url):
         c= row.find_all("td") #encontrame todos los <td>, esto es una lista con todas las columnas de una fila (fecha,hash,tipo,autor,etc...)
         #print(c[0].string[10:13], c[1].string[0:200]) #print(c[0])
 
-        samples.append([c[0].string[0:14],c[1].string[0:200]]) #guardame la columna fecha y hash por c/fila
+        samples.append([c[0].string[0:17],c[1].string[0:200]]) #guardame la columna fecha y hash por c/fila
         
   return samples
 
@@ -121,14 +127,23 @@ def malbazaar():
 
   data=data.dropna() #hay que eliminar unos missing values que surgen de la concatenacion y no aportan nada
   
-  dataHoy=data[(data.Fecha.str[:4]=="2022") & (data.Fecha.str[5:7]==mes) & (data.Fecha.str[8:10]==dia)]
+  #los IOC de hoy
+  dataHoy=data[(data.Fecha.str[:4]==anio) & (data.Fecha.str[5:7]==mes) & (data.Fecha.str[8:10]==dia)]
   
+  #si es Lunes, agarrame los del Sabado y domingo
   if(datetime.datetime.today().weekday()==0):
-    dataDomingo=data[(data.Fecha.str[:4]=="2022") & (data.Fecha.str[5:7]==mes)  & (data.Fecha.str[8:10]==str(int(dia)-1))]
-    dataSabado=data[(data.Fecha.str[:4]=="2022") & (data.Fecha.str[5:7]==mes)  & (data.Fecha.str[8:10]==str(int(dia)-2))]
+    dataDomingo=data[(data.Fecha.str[:4]==anio) & (data.Fecha.str[5:7]==mes)  & (data.Fecha.str[8:10]==str(int(dia)-1))]
+    dataSabado=data[(data.Fecha.str[:4]==anio) & (data.Fecha.str[5:7]==mes)  & (data.Fecha.str[8:10]==str(int(dia)-2))]
     dataAyer=pd.concat([dataDomingo,dataSabado])
+
+  #si es dia de semana, traeme los de ayer, ya sea: si lo ejecute a las 10:20, los que se subieron de 10:20 a 11:00 y de 11:00 a el fin del dia. 
   else:
-    dataAyer=data[(data.Fecha.str[:4]=="2022") & (data.Fecha.str[5:7]==mes)  & (data.Fecha.str[8:10]==str(int(dia)-1)) & (data.Fecha.str[10:13].astype(int)>int(hora))]
+    print(data.Fecha.str[14:16])
+    dataAyerOtrasHoras=data[(data.Fecha.str[:4]==anio) & (data.Fecha.str[5:7]==mes)  & (data.Fecha.str[8:10]==str(int(dia)-1)) & (data.Fecha.str[10:13].astype(int)>int(hora))]
+    dataAyerOtrosMinutos=data[(data.Fecha.str[:4]==anio) & (data.Fecha.str[5:7]==mes)  & (data.Fecha.str[8:10]==str(int(dia)-1)) & (data.Fecha.str[10:13].astype(int)==int(hora)) & (data.Fecha.str[14:16].astype(int)>int(minutos))]
+    dataAyer=pd.concat([dataAyerOtrasHoras,dataAyerOtrosMinutos])
+    
+
 
   dataRequerida=pd.concat([dataHoy,dataAyer])
 
@@ -162,7 +177,7 @@ def banner():
     
 		>Collections: Public Sources                                                                                               
                 >FeodoTracker & MalwareBazaar                                                                                   
-                >v2.0 FullAuto 2022-05-16                                                                                   
+                >v2.1 FullAuto 2022-05-16                                                                                   
                
     '''
 
@@ -177,15 +192,17 @@ banner()
 
 dia=formatoFecha(datetime.datetime.today().day)
 mes=formatoFecha(datetime.datetime.today().month)
+anio=str(datetime.datetime.today().year)
 hora="00"
-
+minutos="00"
 
 print("[AutoPublic]: Hoy es "+datetime.datetime.today().strftime('%Y-%m-%d'))
 print(" ")
 if(datetime.datetime.today().weekday()!=0):
-   print("<?>: Ingresa la hora en la que se ejecuto AutoPublic ayer (hh): ")
+   print("<?>: Ingresa la hora en la que se ejecuto AutoPublic ayer (hh:mm): ")
    hora=input()
-   hora=str(int(hora)+3) #UTC diferencia horaria
+   minutos= hora[3:]
+   hora=str(int(hora[:2])+3) #sumar el UTC de diferencia horaria
 
 print("<?>: Ingresa el nombre del CSV a guardar (sin .csv) ")
 archivo=input()
